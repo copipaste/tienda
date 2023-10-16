fa5a8f430956bdedb7c07d63175726267e053a87
jhoel debray quispe ticona <jhoel.quispe@ficct.uagrm.edu.bo>
2023-10-16 08:51:48 -0400

    1er commit

---
+++ app/Livewire/Search.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\Product;
+
+class Search extends Component
+{
+    public $search;
+
+    public $open = false;
+    public function updatedSearch($value){
+        if ($value) {
+            $this->open = true;
+        }else{
+            $this->open = false;
+        }
+    }
+    public function render()
+    {
+        if ($this->search) {
+            $products = Product::where('name', 'LIKE' ,'%' . $this->search . '%')
+                                ->where('status', 2)
+                                ->take(8)
+                                ->get();
+        } else {
+            $products = [];
+        }
+        
+        return view('livewire.search', compact('products'));
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.js"></script>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glider-js@1/glider.min.css">
+        @livewireStyles
+    </head>
+    <body class="font-sans antialiased">
+        <x-banner />
+
+        <div class="min-h-screen bg-gray-100">
+            @livewire('navigation')
+
+   
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+
+        @stack('modals')
+
+        @livewireScripts
+
+        <script>
+            function dropdown(){
+                return {
+                    open: true,
+                    show(){
+                        if(this.open){
+                            //Se cierra el menu
+                            this.open = false;
+                            document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                        }else{
+                            //Esta abriendo el menu
+                            this.open = true;
+                            document.getElementsByTagName('html')[0].style.overflow = 'hidden'
+                        }
+                    },
+                    close(){
+                        this.open = false;
+                        document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                    }
+                }
+            }
+        </script>
+        @stack('script')
+    </body>
+</html>
